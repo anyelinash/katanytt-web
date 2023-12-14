3d6113061b20bbbb768cafabac0bc9972d64d759
branyerbjr <branyerbjr@gmail.com>
2023-12-13 23:15:55 -0500

    modulo sensor

---
+++ src/Views/HomeAdmin/Aire/ModuloSensor.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import API_CONFIG from '../../../Apis/settings';
+
+
+const ModuloSensor = () => {
+    return(
+
+        <div>
+            
+        </div>
+    );
+};
+
+export default ModuloSensor;
